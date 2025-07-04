3ba81ef8acaab4df741dfc5310fb8facca6b3fd2
Milestone Paite <crepybozz@gmail.com>
2025-07-04 17:46:15 +0530

    first commit

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 import csv
+import os
 
 @app.route('/')
 def home_page():
@@ -17,14 +18,19 @@ def write_to_file(data):
                 message = data['message']
                 file = database.write(f'\n{email}, {subject}, {message}')
 
+
 def write_to_csv(data):
-    with open('database.csv', mode='a', newline='') as database2:  # <-- newline here
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
+    # Always write to the CSV inside the same folder as server.py
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(base_dir, 'portfo', 'database.csv')
+
+    email = data['email']
+    subject = data['subject']
+    message = data['message']
+
+    with open(file_path, mode='a', newline='') as database2:
         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email, subject, message])
-
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
           if request.method == 'POST':
